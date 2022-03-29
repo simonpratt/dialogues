70c74cd6c0a6997bc15243634fe8351624812022
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-03-29 20:03:18 +1030

    Pruned some deps

--- src/components/Page.stories.tsx
+++ src/components/Page.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
 import { Page } from './Page';
 
 export default {
@@ -17,10 +16,3 @@ const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
 export const LoggedOut = Template.bind({});
 
 export const LoggedIn = Template.bind({});
-
-// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
-};
